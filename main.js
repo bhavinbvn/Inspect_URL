376383028b180d23ada3b88f8f41b92f6f0a0a35
bhavinbvn <114050353+bhavinbvn@users.noreply.github.com>
2025-01-09 14:53:53 +0530

    main

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
-import validator from 'validator'
-import fs from 'fs'
-import chalk from 'chalk'
+import validator from 'validator';
+import fs from 'fs';
+import chalk from 'chalk';
+import { URL } from 'url';
 
-let url = process.argv[2]
+let url = process.argv[2];
 
-if(validator.isURL(url)){
-    console.log(chalk.green.bold('The URL is valid.'))
-    fs.appendFile('url_list.txt', `${url}\n`,(err)=>{
-        if(err){
-            throw err
-        }
-    })
-    fs.readFile('block_url.txt', 'utf8',(err,data)=>{
-        if(err){
-            throw err
-        }
+if (validator.isURL(url)) {
+    console.log(chalk.green.bold('The URL is valid.'));
+
+    let searchParams = new URL(url).searchParams;
+
+    if (searchParams.toString()) {
+
+        fs.appendFile('search_params.txt', `${searchParams.toString()}\n`, (err) => {
+            if (err) throw err;
+            console.log(chalk.green('Search parameters saved to search_params.txt'));
+        });
+    } else {
+        console.log(chalk.yellow('No search parameters found in the URL.'));
+    }
+
+    fs.appendFile('url_list.txt', `${url}\n`, (err) => {
+        if (err) throw err;
+    });
+
+
+    fs.readFile('block_url.txt', 'utf8', (err, data) => {
+        if (err) throw err;
 
         let block_urls = data.split('\n');
 
         if (block_urls.includes(url)) {
             console.log(chalk.blue.bold('The URL is blocked.'));
 
-            fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, fileData) {
-                if (err) {
-                    console.log(err);
-                } else {
+    
+            fs.readFile('myjsonfile.json', 'utf8', (readErr, fileData) => {
+                if (readErr) console.log(readErr);
+                else {
                     let obj = {};
-                
-                    // Parse the JSON file or initialize an empty object
+
                     try {
                         obj = JSON.parse(fileData);
                     } catch (parseError) {
@@ -35,15 +46,13 @@ if(validator.isURL(url)){
                         obj = {};
                     }
 
-                   
                     if (obj[url]) {
                         obj[url].count += 1;
                     } else {
                         obj[url] = { count: 1 };
                     }
 
-                    
-                    let json = JSON.stringify(obj, null, 2); 
+                    let json = JSON.stringify(obj, null, 2);
                     fs.writeFile('myjsonfile.json', json, 'utf8', (writeErr) => {
                         if (writeErr) console.log(writeErr);
                         else console.log(chalk.green.bold('JSON file updated successfully.'));
@@ -51,7 +60,7 @@ if(validator.isURL(url)){
                 }
             });
         }
-    })
-}else{
-    console.log(chalk.red.bold('The URL is invalid.'))
+    });
+} else {
+    console.log(chalk.red.bold('The URL is invalid.'));
 }

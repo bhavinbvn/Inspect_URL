c07b24c69e06bf8580579ed09a078de02f0db07d
bhavinbvn <114050353+bhavinbvn@users.noreply.github.com>
2025-01-09 14:26:12 +0530

    main

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import validator from 'validator'
 import fs from 'fs'
+import chalk from 'chalk'
 
 let url = process.argv[2]
 
 if(validator.isURL(url)){
-    console.log('The URL is valid.')
-    fs.appendFile('url_list.txt', url,(err)=>{
+    console.log(chalk.green.bold('The URL is valid.'))
+    fs.appendFile('url_list.txt', `${url}\n`,(err)=>{
         if(err){
             throw err
         }
@@ -18,7 +19,7 @@ if(validator.isURL(url)){
         let block_urls = data.split('\n');
 
         if (block_urls.includes(url)) {
-            console.log('The URL is blocked.');
+            console.log(chalk.blue.bold('The URL is blocked.'));
 
             fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, fileData) {
                 if (err) {
@@ -30,7 +31,7 @@ if(validator.isURL(url)){
                     try {
                         obj = JSON.parse(fileData);
                     } catch (parseError) {
-                        console.log('JSON parse error. Initializing a new object.');
+                        console.log(chalk.red('JSON parse error. Initializing a new object.'));
                         obj = {};
                     }
 
@@ -45,12 +46,12 @@ if(validator.isURL(url)){
                     let json = JSON.stringify(obj, null, 2); 
                     fs.writeFile('myjsonfile.json', json, 'utf8', (writeErr) => {
                         if (writeErr) console.log(writeErr);
-                        else console.log('JSON file updated successfully.');
+                        else console.log(chalk.green.bold('JSON file updated successfully.'));
                     });
                 }
             });
         }
     })
 }else{
-    console.log('The URL is invalid.')
+    console.log(chalk.red.bold('The URL is invalid.'))
 }
